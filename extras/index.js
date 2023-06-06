f4d2b78dabd650c7be974264a3d3f30e8c6bf271
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2023-06-06 16:39:51 +0200

    init

--- extras/index.js
+++ extras/index.js
@@ -7,14 +7,14 @@ app.use(bodyParser.json())
 
 const Storage = multer.diskStorage({
     destination(req, file, callback) {
-        callback(null, './images')
+        callback(null, './public/images')
     },
     filename(req, file, callback) {
         callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`)
     },
 })
 
-const upload = multer({ storage: Storage })
+const upload = multer({ storage: Storage });
 
 app.post('/upload', upload.array('photo', 3), (req, res) => {
     console.log('file', req.files)
@@ -22,8 +22,8 @@ app.post('/upload', upload.array('photo', 3), (req, res) => {
     res.status(200).json({
         message: 'success!',
     })
-})
+});
 
-app.listen(3000, () => {
-    console.log('App running on http://localhost:3001')
-})
+app.listen(3001, () => {
+    console.log('App running on http://localhost:3001');
+});
