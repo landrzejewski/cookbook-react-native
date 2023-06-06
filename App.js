59624be33f2689c1b3e17d2d26b063076aac7aae
Łukasz Andrzejewski <landrzejewski.poczta@gmail.com>
2023-06-06 11:44:14 +0200

    init

--- App.js
+++ App.js
@@ -4,17 +4,40 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MenuScreen from "./src/features/restaurant/MenuScreen";
 import OrderScreen from "./src/features/restaurant/OrderScreen";
+import ProfileScreen from "./src/features/profile/ProfileScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { ShoppingBagIcon, UserIcon } from "react-native-heroicons/outline";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const FoodStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="MenuScreen" component={MenuScreen} options={{ title: "Menu" }} />
+    <Stack.Screen name="OrderScreen" component={OrderScreen} options={{ title: "Order" }} />
+  </Stack.Navigator>
+);
+
+const ProfileStack = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="ProfileScreen" component={ProfileScreen} options={{ title: "Profile" }} />
+  </Stack.Navigator>
+);
+
+const getColor = (isFocused) => isFocused ? '#4775f2' : '#b8bece';
+
+const foodTab = ({focused}) => <ShoppingBagIcon style={{color: getColor(focused)}}/>;
+
+const profileTab = ({focused}) => <UserIcon style={{color: getColor(focused)}}/>;
 
 function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="MenuScreen" component={MenuScreen} options={{ title: "Menu" }} />
-          <Stack.Screen name="OrderScreen" component={OrderScreen} options={{ title: "Order" }} />
-        </Stack.Navigator>
+        <Tab.Navigator screenOptions={{headerShown: false}}>
+          <Tab.Screen name="FoodStack" component={FoodStack} options={{tabBarIcon: foodTab}} />
+          <Tab.Screen name="ProfileStack" component={ProfileStack} options={{tabBarIcon: profileTab}}/>
+        </Tab.Navigator>
       </NavigationContainer>
     </Provider>
   );
